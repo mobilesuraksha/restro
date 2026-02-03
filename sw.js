9fb59f3f886ff1e1be604d80cab1a94e5b3232fa
mobilesuraksha <rishabhranapratap1998@gmail.com>
2026-02-03 22:02:35 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,14 @@
-const CACHE_NAME = "rk-restaurant-v1";
+const CACHE_NAME = "rk-system-v2";
 const urlsToCache = [
-  "./mobile.html",
-  "./manifest.json",
+  "admin.html", "mobile.html", "entry.html", "tv.html",
   "https://cdn.tailwindcss.com",
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
